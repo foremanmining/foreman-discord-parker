d76bd6ee32e2396b639795b04f8610118607c6d6
Dan Lawrence <delawr0190@gmail.com>
2020-01-11 10:46:14 -0500

    Initial project structure

---
+++ src/test/java/mn/foreman/parker/ParkerApplicationTests.java
@@ -0,0 +1,15 @@
+package mn.foreman.parker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/** Unit tests for {@link ParkerApplication}. */
+@SpringBootTest
+class ParkerApplicationTests {
+
+    /** Tests that the context loads. */
+    @Test
+    void contextLoads() {
+        // Do nothing
+    }
+}
